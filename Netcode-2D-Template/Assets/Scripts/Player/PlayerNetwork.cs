a3b87107c5f8aaf8514f0d5241a9c67f42a600a0
Joshua Edgerton <43973457+Joshua-Edgerton@users.noreply.github.com>
2023-01-30 15:01:43 -0600

    Started mouse aim

--- Netcode-2D-Template/Assets/Scripts/Player/PlayerNetwork.cs
+++ Netcode-2D-Template/Assets/Scripts/Player/PlayerNetwork.cs
@@ -9,10 +9,12 @@ public class PlayerNetwork : NetworkBehaviour
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private Vector2 _movementInput;
     [SerializeField] private float _movementSpeed;
+    [SerializeField] private Transform aimTransform;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        aimTransform = transform.Find("AimedParent");
     }
 
     private void FixedUpdate() 

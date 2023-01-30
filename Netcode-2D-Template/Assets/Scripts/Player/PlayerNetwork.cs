618065ea1e50966684466574345939a7852ddc45
Joshua Edgerton <43973457+Joshua-Edgerton@users.noreply.github.com>
2023-01-30 16:39:15 -0600

    Custom Cursor Funtional

--- Netcode-2D-Template/Assets/Scripts/Player/PlayerNetwork.cs
+++ Netcode-2D-Template/Assets/Scripts/Player/PlayerNetwork.cs
@@ -10,17 +10,23 @@ public class PlayerNetwork : NetworkBehaviour
     [SerializeField] private Vector2 _movementInput;
     [SerializeField] private float _movementSpeed;
     [SerializeField] private Transform aimTransform;
+    [SerializeField] private Transform customCursorTransform;
+    [SerializeField] private Vector3 mousePosition;
+    [SerializeField] private SpriteRenderer cursorSpriteRenderer;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         aimTransform = transform.Find("AimedParent");
+        customCursorTransform = transform.Find("PlayerCursor");
+        cursorSpriteRenderer = GameObject.Find("CursorSprite").GetComponent<SpriteRenderer>();
     }
 
     private void Update() 
     {
         if (IsLocalPlayer) 
         { 
+            CustomCursor();
             MousePositionHandler();
         }
     }
@@ -41,9 +47,16 @@ public class PlayerNetwork : NetworkBehaviour
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         // Sets the z axis to 0
         mouseWorldPosition.z = 0f;
+        mousePosition = mouseWorldPosition;
         Vector3 aimDirection = (mouseWorldPosition - transform.position).normalized;
         // Convert to a Euler angle
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    public void CustomCursor() 
+    {
+        Cursor.visible = false;
+        customCursorTransform.position = mousePosition;
     }
 }
